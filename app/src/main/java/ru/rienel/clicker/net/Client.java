be25f43d05bfb3de30c9716f9834201191990f2d
Rienel <mav199824@gmail.com>
2019-03-04 23:35:28 +0300

    [#7] Add SignalAsyncTask, Client and Server Class

---
+++ app/src/main/java/ru/rienel/clicker/net/Client.java
@@ -0,0 +1,4 @@
+package ru.rienel.clicker.net;
+
+public class Client {
+}
---
+++ app/src/main/java/ru/rienel/clicker/net/task/SignalAsyncTask.java
@@ -0,0 +1,23 @@
+package ru.rienel.clicker.net.task;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class SignalAsyncTask extends AsyncTask {
+	private Context context;
+
+	@Override
+	protected Object doInBackground(Object[] objects) {
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(Object o) {
+		super.onPostExecute(o);
+	}
+
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+	}
+}
