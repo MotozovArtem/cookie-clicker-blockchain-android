35459d5f043e718173b8f0b808c3a922d38b766f
Shelgas <akozlov1997@gmail.com>
2019-04-05 16:39:56 +0300

    [#17] - redesign2.0

--- app/src/main/java/ru/rienel/clicker/ui/dialog/ComingSoonDialogFragment.java
+++ app/src/main/java/ru/rienel/clicker/ui/dialog/ComingSoonDialogFragment.java
@@ -4,16 +4,23 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import ru.rienel.clicker.R;
 
 
 public class ComingSoonDialogFragment extends DialogFragment {
 
+    private View view;
+
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-        return builder
-                .setView(R.layout.dialog_coming_soon)
+        view = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_coming_soon, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
                 .create();
     }
 }
